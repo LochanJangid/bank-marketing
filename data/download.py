a840539f57d297885d8f03220b452e34f7842093
lochanjangid <lochanjangidcoder@gmail.com>
2026-09-23 19:11:17 +0000

    feat: download bank marketing dataset from UCI and save as CSV

---
+++ data/download.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+from ucimlrepo import fetch_ucirepo
+
+# Get raw dataset from UC Irvine ML REPO
+bank_marketing = fetch_ucirepo(id=222)
+X = bank_marketing.data.features
+y = bank_marketing.data.targets
+
+# Defined path where we will save our raw dataset
+X_RAW_PATH = os.path.join(os.getcwd(), "data", "raw", "X.csv")
+y_RAW_PATH = os.path.join(os.getcwd(), "data", "raw", "y.csv")
+
+# Make raw folder if not exists
+os.makedirs(os.path.dirname(X_RAW_PATH), exist_ok=True)
+
+# Time to save X, y into csv's files
+X.to_csv(X_RAW_PATH, index=False)
+y.to_csv(y_RAW_PATH, index=False)
