2cc14d49a736ac59ba51974c18938114063d9d24
lochanjangid <lochanjangidcoder@gmail.com>
2026-09-24 06:50:21 +0000

    feat: wrap code into download_data function

--- data/download.py
+++ data/download.py
@@ -2,18 +2,22 @@ import os
 import pandas as pd
 from ucimlrepo import fetch_ucirepo
 
-# Get raw dataset from UC Irvine ML REPO
-bank_marketing = fetch_ucirepo(id=222)
-X = bank_marketing.data.features
-y = bank_marketing.data.targets
+def download_data():
+    # Get raw dataset from UC Irvine ML REPO
+    bank_marketing = fetch_ucirepo(id=222)
+    X = bank_marketing.data.features
+    y = bank_marketing.data.targets
 
-# Defined path where we will save our raw dataset
-X_RAW_PATH = os.path.join(os.getcwd(), "data", "raw", "X.csv")
-y_RAW_PATH = os.path.join(os.getcwd(), "data", "raw", "y.csv")
+    # Defined path where we will save our raw dataset
+    X_RAW_PATH = os.path.join(os.getcwd(), "data", "raw", "X.csv")
+    y_RAW_PATH = os.path.join(os.getcwd(), "data", "raw", "y.csv")
 
-# Make raw folder if not exists
-os.makedirs(os.path.dirname(X_RAW_PATH), exist_ok=True)
+    # Make raw folder if not exists
+    os.makedirs(os.path.dirname(X_RAW_PATH), exist_ok=True)
 
-# Time to save X, y into csv's files
-X.to_csv(X_RAW_PATH, index=False)
-y.to_csv(y_RAW_PATH, index=False)
+    # Time to save X, y into csv's files
+    X.to_csv(X_RAW_PATH, index=False)
+    y.to_csv(y_RAW_PATH, index=False)
+
+if __name__ == "__main__":
+    download_data()
